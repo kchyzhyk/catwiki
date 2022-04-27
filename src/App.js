43eeb6eb01cf487a00aa263ec45a24371f386dc0
kchyzhyk <kchyzhykova888@gmail.com>
2022-04-27 16:45:06 +0300

    +initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {ReactComponent as Logo} from '../src/css/img/logo.svg'
+import IntroLogo from '../src/css/img/logo-white.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <header className="App-header">
+              <a href="/">
+                  <Logo />
+              </a>
+          </header>
+              <div className="intro">
+                  <img src={IntroLogo} alt="" className="whiteLogo" />
+                  <div className="introText">
+                      Get to know more about your <br/> cat breed
+                  </div>
+              </div>
+      </div>
     </div>
   );
 }
