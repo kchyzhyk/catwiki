55da2fbdb6ebdb484b68437ab976b4bd5ca5f87d
kchyzhyk <kchyzhykova888@gmail.com>
2022-04-29 14:58:35 +0300

    +input

--- src/components/SearchBreeds.js
+++ src/components/SearchBreeds.js
@@ -4,39 +4,42 @@ import {getAllBreeds} from "../api/store";
 import {Link} from "react-router-dom";
 
 export const SearchBreeds = () => {
+    const [getOne, setGetOne] = useState([])
     const [allBreeds, setAllBreeds] = useState([
-        'Book',
-        'pear',
-        'l.addams',
-        'George',
-        'Bengal',
-        'trip',
-        'reason',
-        'near',
-        'wave',
-        'concerned',
-        'drain',
-        'vast',
-        'use',
-        'beam',
-        'massive',
-        'volcano',
-        'servant'
+        // 'Book',
+        // 'pear',
+        // 'l.addams',
+        // 'George',
+        // 'Bengal',
+        // 'trip',
+        // 'reason',
+        // 'near',
+        // 'wave',
+        // 'concerned',
+        // 'drain',
+        // 'vast',
+        // 'use',
+        // 'beam',
+        // 'massive',
+        // 'volcano',
+        // 'servant'
     ])
 
-    // useEffect(() => {
-    //     getAllBreeds().then(r => setAllBreeds(r.map((it) => it.name)))
-    // }, [])
+    useEffect(() => {
+        getAllBreeds().then(r => setAllBreeds(r.map((it) => it.name)))
+    }, [])
 
     const handleChange = (e) => {
-        const value = e.target.value.toLowerCase().split('');
+        const value = e.target.value.toLowerCase();
         console.log(value)
-
-        allBreeds.filter((item) => item.toLowerCase().includes(value) ? console.log('here i am', item) : item)
+        // const results = new Array()
+        allBreeds.map((item) => item.toLowerCase().includes(value) ? console.log('results', item) : item)
+        // console.log('results', results)
     }
 
     return (
         <form className="form">
+            {/*{console.log(getOne)}*/}
             <input type="text"
                    className="search"
                    placeholder="Enter your breed"
@@ -44,10 +47,10 @@ export const SearchBreeds = () => {
             />
             <SearchIcon className="search-icon"/>
 
-            <div className="names-list hidden">
-                <ul className="names-list hidden">
+            <div className="names-list">
+                <ul>
                     {allBreeds.map((name, index) =>
-                        <Link to={`/breeds/search/${name}`}>
+                        <Link className="list-item" to={`/breeds/search/${name}`}>
                             <li key={index}> {name} </li>
                         </Link>
                     )}
